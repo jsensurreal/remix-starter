0a229d98a035c010b9ee11d4f7dc69e391d5b27a
Jeffrey Enriquez <jsensurreal@gmail.com>
2024-05-23 18:46:38 -0400

    Initial commit

---
+++ vite.config.js
@@ -0,0 +1,20 @@
+import path from "path";
+import { vitePlugin as remix } from "@remix-run/dev";
+import { defineConfig } from "vite";
+
+export default defineConfig({
+  plugins: [
+    remix({
+      future: {
+        v3_fetcherPersist: true,
+        v3_relativeSplatPath: true,
+        v3_throwAbortReason: true,
+      },
+    }),
+  ],
+  resolve: {
+    alias: {
+      "~": path.resolve(__dirname, "./app"),
+    },
+  },
+});
